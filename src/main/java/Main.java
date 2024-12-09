941b422d81ec72ad06384d057252675902e943f1
ofrii08 <ofrikal2008@gmail.com>
2024-12-09 14:27:49 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,6 +25,86 @@ public class Member {
         return count;
     }
 
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void printListReverse(Node<Integer> head) {
+        if (head == null) {
+            return;
+        }
+        printListReverse(head.getNext());
+        System.out.println(head.getValue());
+    }
+    
+    public static void printEvenNumbers(Node<Integer> head) {
+        if (head == null) {
+            return;
+        }
+        if (head.getValue() != null && head.getValue() % 2 == 0) {
+            System.out.println(head.getValue());
+        }
+        printEvenNumbers(head.getNext());
+    }
+
+    public static Node<Integer> createListFromInput() {
+        Node<Integer> list = new Node<>(null);
+        Node<Integer> p = list;
+        System.out.println("הכנס מספרים או -1 כדי לסיים:");
+        int num = scanner.nextInt();
+        while (num != -1) {
+            Node<Integer> newNode = new Node<>(num);
+            p.setNext(newNode);
+            p = newNode;
+            System.out.println("הכנס מספרים או -1 כדי לסיים:");
+            num = scanner.nextInt();
+        }
+        return list.getNext();
+    }
+
+    public static boolean containsValueRecursive(Node<Integer> head, Object target) {
+        if (head == null) {
+            return false;
+        }
+        if (head.getValue().equals(target)) {
+            return true;
+        }
+        return containsValueRecursive(head.getNext(), target);
+    }
+
+    public static Node<Integer> createListFromArray(int[] array) {
+        Node<Integer> head = new Node<>(array[0]);
+        Node<Integer> p = head;
+        for (int i = 1; i < array.length; i++) {
+            Node<Integer> newNode = new Node<>(array[i]);
+            p.setNext(newNode);
+            p = newNode;
+        }
+        return head;
+    }
+
+    public static void printListIteratively(Node<Integer> head) {
+        while (head != null) {
+            System.out.println(head.getValue());
+            head = head.getNext();
+        }
+    }
+
+    public static void printListReverse2(Node<Integer> head) {
+        if (head == null) {
+            return;
+        }
+        printListReverse2(head.getNext());
+        System.out.println(head.getValue());
+    }
+
+    public static boolean containsValueIteratively(Node<Integer> head, int target) {
+        while (head != null) {
+            if (head.getValue() == target) {
+                return true;
+            }
+            head = head.getNext();
+        }
+        return false;
+    }
     public static void main(String[] args) {
         int x = 1; 
         int y = 100; 
@@ -37,5 +117,35 @@ public class Member {
         System.out.println("המספר " + numberToCount + " מופיע " + occurrences + " פעמים ברשימה");
     }
 }
+    public static void main(String[] args) {
+        int[] array = {10, 15, 20, 25, 30};
+        Node<Integer> listFromArray = createListFromArray(array);
+        System.out.println("רשימה שנבנתה ממערך:");
+        printListIteratively(listFromArray);
+
+        System.out.println("\nהדפסת הרשימה הפוך:");
+        printListReverse(listFromArray);
+
+        System.out.println("\nהמספרים הזוגיים ברשימה:");
+        printEvenNumbers(listFromArray);
+
+        System.out.println("\nהכנס ערכים ליצירת רשימה:");
+        Node<Integer> userList = createListFromInput();
+        System.out.println("הרשימה שנוצרה מהקלט שלך:");
+        printListIteratively(userList);
+
+        System.out.println("\nהכנס מספר לחיפוש ברשימה (רקורסיה):");
+        int target = scanner.nextInt();
+        boolean foundRecursively = containsValueRecursive(listFromArray, target);
+        System.out.println("האם המספר נמצא ברשימה? " + foundRecursively);
+
+        System.out.println("\nהכנס מספר לחיפוש ברשימה (איטרטיבי):");
+        target = scanner.nextInt();
+        boolean foundIteratively = containsValueIteratively(listFromArray, target);
+        System.out.println("האם המספר נמצא ברשימה? " + foundIteratively);
+    }
+}
+
+
 
 }
